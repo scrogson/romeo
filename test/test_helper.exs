b98372a1e4d4e543ceac4a0eadc61599c15831e4
Sonny Scroggin <sonny@scrogg.in>
2019-09-02 22:50:06 -0500

    add more time

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
 Logger.configure(level: :info)
 Code.require_file("user_helper.exs", __DIR__)
-ExUnit.configure(assert_receive_timeout: 2000)
+ExUnit.configure(assert_receive_timeout: 5000)
 ExUnit.start()
